7b06cc0daa30076de10c665a3661fed1f56bab1b
karma104 <117033709+karma104@users.noreply.github.com>
2022-10-31 00:55:51 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,47 @@ module.exports = function(eleventyConfig) {
     'node_modules/nes.css/css': 'assets/nes.css'
   });
 
+
+async function evaluatePixelChanges(jsonPatch) {
+  if (!doesJsonMatchSchema(jsonPatch.after)) {
+    fail(stripIndents`Your pixels.json file does not match the expected schema.
+
+    expected schema: 
+    {
+      data: [
+        // pixels go here
+      ]
+    }
+    `);
+    return false;
+  }
+
+function convertJsonToPixelSet({ data }) {
+  const pixelSet = new Set();
+  data.forEach(pixel => pixelSet.add(JSON.stringify(pixel)));
+
+  return pixelSet;
+}
+
+async function evaluatePixelChanges(jsonPatch) {
+  if (!doesJsonMatchSchema(jsonPatch.after)) {
+    fail(stripIndents`Your pixels.json file does not match the expected schema.
+
+    expected schema: 
+    {
+      data: [
+        // pixels go here
+      ]
+    }
+    `);
+    return false;
+  }
+
+  const isDiffEmpty = jsonPatch.diff.length === 0;
+  if (isDiffEmpty) {
+    fail('This PR appears to be empty and needs a manual review.');
+    return false;
+  }
   // This is so we can expose the docker port and host to be accesible
   if (fs.existsSync(dockerEnvFile)) {
     eleventyConfig.setBrowserSyncConfig({
